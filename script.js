2f729e97c80c197494fb5acd83dcfecc76704558
Marcos Paulo <103538721+KingZabitus@users.noreply.github.com>
2023-02-27 19:52:16 -0300

    Incrementando a função de Brincar
    Adicionando um minigame de Pedra-Papel-Tesoura na função de Brincar do bichinho virtual.

--- script.js
+++ script.js
@@ -4,9 +4,101 @@ let sono = 100;
 let diversao = 100;
 let social = 100;
 let treino = 0;
+let total = fome + higiene + sono + diversao + social;
+
+//Código relacionado ao minigame de Pedra-Papel-Tesoura
+let choices = ["pedra", "papel", "tesoura"];
+let userChoice = "";
+let resultado = 0;
+let tesoura = document.createElement('button');
+tesoura.innerHTML = "Tesoura";
+tesoura.classList.add("botaoDeTesoura");
+tesoura.style.display = "none";
+document.body.appendChild(tesoura);
+
+let pedra = document.createElement('button');
+pedra.innerHTML = "Pedra";
+pedra.classList.add("botaoDePedra");
+pedra.style.display = "none";
+document.body.appendChild(pedra);
+
+let papel = document.createElement('button');
+papel.innerHTML = "Papel";
+papel.classList.add("botaoDePapel");
+papel.style.display = "none";
+document.body.appendChild(papel);
+
+let brincarContainer = document.getElementById('brincarContainer');
+brincarContainer.appendChild(papel, pedra, tesoura);
+
+let computerChoice = "";
+let jogadaFeita = false;
+
+function escolher() {
+    if (jogadaFeita) return;
+    jogadaFeita = true;
+    tesoura.style.display = "block";
+    pedra.style.display = "block";
+    papel.style.display = "block";
+
+    tesoura.addEventListener("click", () => {
+        userChoice = "tesoura";
+        pedra.style.display = "none";
+        papel.style.display = "none";
+        tesoura.style.display = "none";
+        computerChoice = choices[Math.floor(Math.random() * choices.length)];
+        resultado = verificarVencedor();
+        diversao = diversao + resultado;
+        document.getElementById("diversão").innerHTML = diversao + '%';
+        document.getElementById("diversão").style.width = diversao + 'px';
+    })
+    pedra.addEventListener("click", () => {
+        userChoice = "pedra";
+        pedra.style.display = "none";
+        papel.style.display = "none";
+        tesoura.style.display = "none";
+        computerChoice = choices[Math.floor(Math.random() * choices.length)];
+        resultado = verificarVencedor();
+        diversao = diversao + resultado;
+        document.getElementById("diversão").innerHTML = diversao + '%';
+        document.getElementById("diversão").style.width = diversao + 'px';
+    })
+    papel.addEventListener("click", () => {
+        userChoice = "papel";
+        pedra.style.display = "none";
+        papel.style.display = "none";
+        tesoura.style.display = "none";
+        computerChoice = choices[Math.floor(Math.random() * choices.length)];
+        resultado = verificarVencedor();
+        diversao = diversao + resultado;
+        document.getElementById("diversão").innerHTML = diversao + '%';
+        document.getElementById("diversão").style.width = diversao + 'px';
+    })
+}
+
+
+function verificarVencedor() {
+    alert("Você escolheu " + userChoice + ".\nO computador escolheu " + computerChoice + ".");
+    if (userChoice === computerChoice) {
+        alert("Empate!");
+        resultado = 0;
+    } else if (userChoice === "pedra" && computerChoice === "tesoura" ||
+        userChoice === "papel" && computerChoice === "pedra" ||
+        userChoice === "tesoura" && computerChoice === "papel") {
+        alert("Você ganhou!");
+        resultado = 30;
+    } else {
+        alert("O computador ganhou!");
+        resultado = 0;
+    }
+    jogadaFeita = false;
+    return resultado;
+}
+
+
+//Resto do código
 
 function Loop(time = 2) {
-    let total = fome + higiene + sono + diversao + social;
     if (fome <= 0 || higiene <= 0 || sono <= 0 || diversao <= 0 || social <= 0) {
         document.getElementById("pet").src = "img/preto.jpg";
         document.getElementById("pet").innerHTML = "Você deixou seu pet morrer"
@@ -33,21 +125,36 @@ function Loop(time = 2) {
     if (fome < 0) {
         fome = 0;
     }
+    if (fome > 100){
+        fome = 100;
+    }
     higiene = higiene - parseInt(time);
     if (higiene < 0) {
         higiene = 0;
+    }
+    if (higiene > 100){
+        higiene = 100;
     }
     sono = sono - parseInt(time);
     if (sono < 0) {
         sono = 0;
     }
+    if (sono > 100){
+        sono = 100;
+    }
     diversao = diversao - parseInt(time);
     if (diversao < 0) {
         diversao = 0;
     }
+    if (diversao > 100){
+        diversao = 100;
+    }
     social = social - parseInt(time);
     if (social < 0) {
         social = 0;
+    }
+    if (social > 100){
+        social = 100;
     }
 
     document.getElementById("fome").innerHTML = fome + '%';
@@ -83,10 +190,8 @@ function Dormir() {
 }
 
 function Brincar() {
-    diversao = 100;
-    document.getElementById("diversão").innerHTML = diversao + '%';
-    document.getElementById("diversão").style.width = diversao + 'px';
-}
+    escolher();
+  }
 
 function Conversar() {
     social = 100;
